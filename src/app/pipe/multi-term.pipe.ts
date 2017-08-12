b3533e9ad2a36abc5642d4b8551e2c6897a8ef5c
Victor Chacon <victorchacon@Victors-Air.fios-router.home>
2017-08-12 01:56:50 -0700

    Cleaned up code. Trimmed filter string to prevent emtpy string element from being in list

--- src/app/pipe/multi-term.pipe.ts
+++ src/app/pipe/multi-term.pipe.ts
@@ -12,25 +12,25 @@ export class MultiTermPipe implements PipeTransform {
       return items;
     }
     debugger;
-    let result = filterString.split(' ');
+    let result = filterString.trim().split(' ');
     console.log(result);
     // filter items array, items which match and return true will be kept, false will be filtered out
-    return this.filterLoop(items, result); 
-    //items.filter(item => {
-      //return item.name.toLowerCase().indexOf(filterString.toLowerCase()) !== -1)
-    };
+    return this.filterLoop(items, result);
+  };
 
-    filterLoop(items, filters): any[] {
-        var filteredArray = items.filter(
-        function(el) { // executed for each item
-            for (var i = 0; i < filters.length; i++) { // iterate over filter
-                if (el.name.toLowerCase().indexOf(filters[i].toLowerCase()) != -1) {
-                    return true; // if this item knows this matches
-                }
-            }
-            return false;
+  filterLoop(items, filters): any[] {
+    let filteredArray = items.filter(
+      function (el) { // executed for each item
+        for (let i = 0; i < filters.length; i++) { // iterate over filter
+          if (el.name.toLowerCase().indexOf(filters[i].toLowerCase()) != -1) {
+            return true; // if this filter matches this item
+          }
         }
-        );     
-        return filteredArray;
-    };
-  }
+
+        return false;
+      }
+    );
+
+    return filteredArray;
+  };
+}
