d715f6e58b5a0091128d1a5d2241aed0f9829b02
Victor Chacon <victorchacon@Victors-Air.fios-router.home>
2017-08-12 01:40:07 -0700

    Added multi parameter filter

--- src/app/pipe/multi-term.pipe.ts
+++ src/app/pipe/multi-term.pipe.ts
@@ -4,16 +4,33 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'multiTermPipe',
   pure: false
 })
+
 export class MultiTermPipe implements PipeTransform {
 
   transform(items: any[], filterString: string): any {
     if (!items || !filterString) {
-        return items;
+      return items;
     }
-    
+    debugger;
     let result = filterString.split(' ');
+    console.log(result);
     // filter items array, items which match and return true will be kept, false will be filtered out
-    return items.filter(item => item.name.indexOf(filterString) !== -1);
-}
+    return this.filterLoop(items, result); 
+    //items.filter(item => {
+      //return item.name.toLowerCase().indexOf(filterString.toLowerCase()) !== -1)
+    };
 
-}
+    filterLoop(items, filters): any[] {
+        var filteredArray = items.filter(
+        function(el) { // executed for each item
+            for (var i = 0; i < filters.length; i++) { // iterate over filter
+                if (el.name.toLowerCase().indexOf(filters[i].toLowerCase()) != -1) {
+                    return true; // if this item knows this matches
+                }
+            }
+            return false;
+        }
+        );     
+        return filteredArray;
+    };
+  }
--- src/app/services/in-memory-data.service.ts
+++ src/app/services/in-memory-data.service.ts
@@ -4,13 +4,13 @@ export class InMemoryDataService implements InMemoryDbService {
     let heroes = [
       {id: 11, name: 'Mrs. Nice'},
       {id: 12, name: 'The Narco'},
-      {id: 13, name: 'Bombasto The Magnificent'},
+      {id: 13, name: 'Bombasto The DeNada'},
       {id: 14, name: 'Mr. Celeritas'},
       {id: 15, name: 'Dr. Magneta'},
       {id: 16, name: 'RubberMan'},
       {id: 17, name: 'Dynamado The Man'},
       {id: 18, name: 'Dr IQ'},
-      {id: 19, name: 'Magma The Grand'},
+      {id: 19, name: 'Magma The Grand Narco'},
       {id: 20, name: 'Tornado'}
     ];
     return {heroes};
